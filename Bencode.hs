1bb9cff76f0a184e7f5efd4623fd3016fbf1a933
Daria Jung <djj2115cu@gmail.com>
2014-06-30 11:30:13 -0400

    implement bencode for val BList

--- Bencode.hs
+++ Bencode.hs
@@ -1,6 +1,7 @@
 module Bencode where
 
 import qualified Data.ByteString as B
+import Data.ByteString.Char8 (pack, unpack)
 import qualified Data.Map as M
 import qualified Data.List as L
 
@@ -12,11 +13,15 @@ import qualified Data.List as L
     dictionaries
 -}
 
+-- should BStr take a normal String, or a ByteString? 
+-- Not sure.
+
 data BValue = BInt Integer
-            | BStr String
+            | BStr B.ByteString
             | BList [BValue]
             | BDict [M.Map B.ByteString BValue]
 
 bencode :: BValue -> String
 bencode (BInt int) = "i" ++ show int ++ "e"
-bencode (BStr str) = show (L.length str) ++ ":" ++ (str)
+bencode (BStr str) = show (B.length str) ++ ":" ++ (unpack str)
+bencode (BList xs) = "l" ++ L.concatMap bencode xs ++ "e"
